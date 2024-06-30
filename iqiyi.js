50494bb904e8afa37ec1acf47937e856748d4ea2
Butcher <755365600@qq.com>
2024-06-30 18:07:55 +0800

    two scripts

---
+++ iqiyi.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         iqiyi 文字可选
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  iqiyi 文字可选
+// @author       Butcher
+// @match        https://www.iqiyi.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=iqiyi.com
+// @grant        none
+// ==/UserScript==
+(function () {
+    "use strict";
+    window.addEventListener("load", () => {
+        setTimeout(() => {
+            const body = document.body;
+            body.style.userSelect = "auto";
+        }, 2000);
+    });
+})();
---
+++ restore-wht-links.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         还原 wht 链接
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  还原 wht 链接
+// @author       Butcher
+// @match        https://0x3.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=0x3.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    // 搜索 desktop
+    var desktop = {
+        "淘宝网": "https://www.taobao.com/",
+        "天猫": "https://www.tmall.com/",
+        "天猫超市": "https://chaoshi.tmall.com/",
+        "京东": "https://www.jd.com/",
+        "唯品会": "https://www.vip.com/",
+        "飞猪旅行": "https://www.fliggy.com/",
+        "携程旅行": "https://www.ctrip.com/",
+        "当当": "https://www.dangdang.com/",
+    }
+
+    setTimeout(() => {
+        const la = document.getElementsByClassName("la")[0];
+        la.addEventListener("click", function (e) {
+            const target = e.target;
+            if (target.tagName === "A" && target.classList.contains("xa")) {
+                const span = target.querySelector("span.Ca");
+                if (desktop.hasOwnProperty(span?.textContent)) {
+                    target.href = desktop[span.textContent];
+                }
+            }
+        })
+    }, 2000);
+})();
